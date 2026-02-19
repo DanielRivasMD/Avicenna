519693a2e5a25bbaa059ab8c379193d8c341f4dd
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-19 13:16:39 +0200

    add repl interfaces

---
+++ src/interfaces/repl.jl
@@ -0,0 +1,26 @@
+# src/interfaces/repl.jl
+module REPL
+
+using ..Workflow
+using ..Processing
+
+function get_cache(no_cache::Bool = false)
+  return Workflow.Cache("cache/demo", !no_cache)
+end
+
+function run_demo(id::String, scale::Float64 = 1.0; no_cache::Bool = false)
+  config = Dict("id" => id, "data" => [1.0, 2.0, 3.0, 4.0, 5.0], "scale" => scale)
+  return Workflow.run(Workflow.demo_workflow, config, cache = get_cache(no_cache))
+end
+
+function inspect_stage(result, stage::String)
+  return result.stage_outputs[stage]
+end
+
+function clear_cache!()
+  rm("cache/demo", recursive = true, force = true)
+  mkpath("cache/demo")
+  @info "Cache cleared"
+end
+
+end
