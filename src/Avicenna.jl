f7893c55343a4e90a8f3adf2ded675ecc84f590d
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-19 13:16:55 +0200

    add module root

---
+++ src/Avicenna.jl
@@ -0,0 +1,16 @@
+# src/Avicenna.jl
+module Avicenna
+
+include("core/processor.jl")
+include("workflows/main_workflow.jl")
+include("interfaces/repl.jl")
+include("interfaces/cli.jl")
+include("interfaces/document.jl")
+
+export Core, Workflow, REPL, CLI, Document
+
+function quick(id::String, scale::Float64 = 1.0)
+  return REPL.run_demo(id, scale)
+end
+
+end
