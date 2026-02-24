1f473f942802a2ce091f587dca08d0e5e84e0156
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-24 13:37:46 +0200

    add html rendering document

--- src/interfaces/document.jl
+++ src/interfaces/document.jl
@@ -7,10 +7,13 @@ module Document
 
 using ..Workflow
 using Markdown
+using Weave
+using UUIDs
+using Base.Filesystem: mktempdir
 
 ####################################################################################################
 
-function generate_report(result)
+function report_markdown(result)
   output = result.stage_outputs["analyze"]
 
   md = """
@@ -33,6 +36,43 @@ function generate_report(result)
   """
 
   return Markdown.parse(md)
+end
+
+####################################################################################################
+
+function report_html(result; outpath = nothing)
+  output = result.stage_outputs["analyze"]
+
+  md = """
+  # Analysis Report
+
+  ## Summary
+  - Mean: $(output.summary["mean"])
+  - Data points: $(output.summary["length"])
+
+  ## Origin
+  - Workflow: $(result.origin["workflow"])
+  - Version: $(result.origin["version"])
+  - Time: $(result.origin["timestamp"])
+  - Cache hits: $(join(result.origin["cache_hits"], ", "))
+
+  ## Configuration
+  ```
+  $(result.origin["config"])
+  ```
+  """
+
+  tmpdir = mktempdir()
+  infile = joinpath(tmpdir, "report.jmd")
+  open(infile, "w") do io
+    write(io, md)
+  end
+
+  outfile = isnothing(outpath) ? joinpath(tmpdir, "report.html") : outpath
+
+  weave(infile; doctype = "md2html", out_path = outfile)
+
+  return outfile
 end
 
 ####################################################################################################
